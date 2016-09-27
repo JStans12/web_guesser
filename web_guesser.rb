e23ea86d80ef776eee2d091d811e1fdd925165e4
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-09-26 20:20:09 -0600

    guess limiting and cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,19 +1,51 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
-number = SECRET_NUMBER
+@@secret_number = rand(100)
+@@remaining_guesses = 6
 
 get '/' do
   guess = params['guess'].to_i
-  message = check_guess(guess)
-  erb :index, :locals => {:number => number, :message => message}
+  cheat = params['cheat'] || false
+  number = @@secret_number
+  guess_message = check_guess(guess)
+  remaining_guesses = count_guesses
+  win_or_lose_message = win_or_lose(guess_message, remaining_guesses)
+
+  erb :index, :locals => {:number => number,
+                          :guess_message => guess_message,
+                          :remaining_guesses => remaining_guesses,
+                          :win_or_lose_message => win_or_lose_message,
+                          :cheat => cheat}
 end
 
 def check_guess(guess)
-  return "Way too high!" if guess > SECRET_NUMBER + 5
-  return "Too high!" if guess > SECRET_NUMBER
-  return "Way too low" if guess < SECRET_NUMBER - 5
-  return "Too low" if guess < SECRET_NUMBER
-  return "Nailed it!" if guess == SECRET_NUMBER
+  return "Way too high!" if guess > @@secret_number + 5
+  return "Too high!"     if guess > @@secret_number
+  return "Way too low"   if guess < @@secret_number - 5
+  return "Too low"       if guess < @@secret_number
+  return "Nailed it!"    if guess == @@secret_number
+end
+
+def count_guesses
+  @@remaining_guesses -= 1
+  return "Out of guesses!" if @@remaining_guesses == 0   
+  return "#{@@remaining_guesses} guesses left."
+end
+
+def win_or_lose(guess_message, remaining_guesses)
+  return win if guess_message == "Nailed it!"
+  return lose if remaining_guesses == "Out of guesses!"
+end
+
+def win
+  @@secret_number = rand(100)
+  @@remaining_guesses = 5
+  "You win!"
+end
+
+def lose
+  @@secret_number = rand (100)
+  @@remaining_guesses = 5
+  "You lose!"
 end
