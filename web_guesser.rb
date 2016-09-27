9fb073d314a476d0aa2323046f51b1085b9257dc
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-09-26 18:01:02 -0600

    game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number = rand(100)
+SECRET_NUMBER = rand(100)
+number = SECRET_NUMBER
 
 get '/' do
-erb :index, :locals => {:secret_number => secret_number}
+  guess = params['guess'].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:number => number, :message => message}
+end
+
+def check_guess(guess)
+  return "Way too high!" if guess > SECRET_NUMBER + 5
+  return "Too high!" if guess > SECRET_NUMBER
+  return "Way too low" if guess < SECRET_NUMBER - 5
+  return "Too low" if guess < SECRET_NUMBER
+  return "Nailed it!" if guess == SECRET_NUMBER
 end
